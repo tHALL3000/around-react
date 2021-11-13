91f6bd2adaffca49ec660349c0aa7fb1755cb537
tHALL3000 <tiffanyhall.co@gmail.com>
2021-11-13 11:39:56 -0700

    small change

--- sprint-10/src/components/App.js
+++ sprint-10/src/components/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './index.css';
+import '../../src/index.css';
 import Header from './Header.js';
 import Main from './Main.js';
 import PopupWithForm from './PopupWithForm.js';
 import Footer from './Footer.js';
 import ImagePopup from './ImagePopup';
-import {api} from '../utils/api';
+// import {api} from '../utils/api';
 
 function App() {
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
